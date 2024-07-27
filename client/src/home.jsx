1e9aaf6b7e80e70a2fae54ba35b0776fb61cc11e
Sakthipriyan V <136572144+SAKTHIPRIYAN1@users.noreply.github.com>
2024-07-27 09:42:19 +0530

    changing url

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -600,7 +600,7 @@ const ContactForm = () => {
   const handleSubmit =async (e) => {
     e.preventDefault();
     try{
-        let resp=await axios.post('http://localhost:3000/sendmail',formData);
+        let resp=await axios.post('https://portfolio-server-da9zdsrgb-sakthipriyans-projects.vercel.app/sendmail',formData);
         toast.success("Mail sent succssfully");
         setFormData({
             name: '',
